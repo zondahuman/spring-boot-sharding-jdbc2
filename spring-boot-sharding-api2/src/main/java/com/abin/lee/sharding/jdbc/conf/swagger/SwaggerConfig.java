db551eb24c1682c17ab28a146045fda55ed6adf2
zondahuman <zondahuman@gmail.com>
2018-08-16 12:35:38 +0800

    add

--- spring-boot-sharding-api2/src/main/java/com/abin/lee/sharding/jdbc/conf/swagger/SwaggerConfig.java
+++ spring-boot-sharding-api2/src/main/java/com/abin/lee/sharding/jdbc/conf/swagger/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.abin.lee.sharding.jdbc.conf.swagger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,26 +15,34 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by abin on 2018/8/16.
+ * http://localhost:9061/swagger-ui.html
+ * http://localhost:9061/api-docs
  */
 @Configuration
 @EnableWebMvc
 @EnableSwagger2
 public class SwaggerConfig {
+    @Autowired
+    private Environment env;
+
     @Bean
     public Docket buildDocket() {
+        String localPort = env.getProperty("server.port");
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(buildApiInf())
-                .host("localhost:8080")
+                .host("localhost"+localPort)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dianping.orderdish.webapi"))//controller路径
+                .apis(RequestHandlerSelectors.basePackage("com.abin.lee.sharding.jdbc.controller"))//controller路径
                 .paths(PathSelectors.any()).build();
     }
 
     private ApiInfo buildApiInf() {
         return new ApiInfoBuilder()
                 .title("RestAPI Docs")
-                .termsOfServiceUrl("http://www.github.com/kongchen/swagger-maven-plugin")
+                .termsOfServiceUrl("https://github.com/zondahuman/spring-boot-sharding-jdbc2")
                 .build();
-
     }
+
+
+
 }
--- spring-boot-sharding-api2/src/main/java/com/abin/lee/sharding/jdbc/controller/OrderController.java
+++ spring-boot-sharding-api2/src/main/java/com/abin/lee/sharding/jdbc/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.abin.lee.sharding.jdbc.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -13,18 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order")
 @Slf4j
+@Api("OrderController--api")
 public class OrderController {
 
     @Autowired
     private Environment env;
 
-
+    @ApiOperation(value = "根据id查询学生信息", notes = "查询数据库中某个的学生信息")
+    @ApiImplicitParam(name = "param", value = "随机参数", paramType = "path", required = true, dataType = "Integer")
     @RequestMapping(value = "/add", method = {RequestMethod.GET, RequestMethod.POST})
     public String add(Integer param) {
         String localParam = env.getProperty("local.param");
         log.info("---------param= " + param + "  , localParam= " + localParam);
         return localParam;
     }
+
+    @ApiOperation(value = "根据id查询订单信息", notes = "查询数据库中某个的订单信息")
+    @ApiImplicitParam(name = "id", value = "订单号", paramType = "path", required = true, dataType = "Integer")
+    @RequestMapping(value = "/find", method = {RequestMethod.GET, RequestMethod.POST})
+    public String find(Long id) {
+
+        log.info("---------param= " + id);
+        return id + "";
+    }
+
+
 
 
 }
