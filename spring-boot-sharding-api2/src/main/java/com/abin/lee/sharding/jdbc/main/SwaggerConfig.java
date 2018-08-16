eaeee8aaae2e40b74ed6ff7df921ecf30d6f6a04
zondahuman <zondahuman@gmail.com>
2018-08-16 13:10:33 +0800

    add

--- spring-boot-sharding-api2/src/main/java/com/abin/lee/sharding/jdbc/main/SwaggerConfig.java
+++ spring-boot-sharding-api2/src/main/java/com/abin/lee/sharding/jdbc/main/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.abin.lee.sharding.jdbc.main;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -18,16 +19,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * Created by abin on 2018/8/16.
  * http://localhost:9061/swagger-ui.html
  * http://localhost:9061/v2/api-docs
+ * 生产环境不启用swagger文档
+ * https://blog.csdn.net/u014087707/article/details/79347153
  */
 @Configuration
 public class SwaggerConfig {
     @Autowired
     private Environment env;
+    @Value("${swagger.show}")
+    private boolean swaggerShow;
 
     @Bean
     public Docket buildDocket() {
         String localPort = env.getProperty("server.port");
         return new Docket(DocumentationType.SWAGGER_2)
+                .enable(swaggerShow)
                 .apiInfo(buildApiInf())
                 .host("localhost"+localPort)
                 .select()
